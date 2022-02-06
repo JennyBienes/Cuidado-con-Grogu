922f20d247abcd340f1ac40debf48b67ef955b7c
Jenny <bienesjenny@gmail.com>
2022-02-06 16:57:26 +0100

    Insert HomePage.js

--- cuidado-con-grogu/src/App.js
+++ cuidado-con-grogu/src/App.js
@@ -3,26 +3,53 @@ import './App.css';
 import { useState } from 'react';
 import Footer from './components/Footer';
 import GameComponent from './components/GameComponent';
-import  SideBar from './components/SideBar';
-import  Main  from './components/Main';
-import  PiecesComponent  from './components/PiecesComponent';
-import  RulesComponent  from './components/RulesComponent';
+import SideBar from './components/SideBar';
+//import  Main  from './components/Main';
+import PiecesComponent from './components/PiecesComponent';
+import RulesComponent from './components/RulesComponent';
+//import HomePage from './components/HomePage';
 
-
-console.log('Entrando a App');
 function App() {
-  return (
-    <div className='App__background'>
-        <div className="App__layout">
-           <div className="App__sidebar">
-           <SideBar/>
-           </div>
-           <div className="App__main">
-            <p> Parrafo o texto a explicar</p>    
-           </div>
+  const [OptionSelected, setOptionSelected] = useState(1);
+
+  if (OptionSelected === 1) {
+    setOptionSelected([1])
+  } else if (OptionSelected === 2){
+    setOptionSelected([2])
+  } else if (OptionSelected === 3){
+  setOptionSelected([3])
+  } else if (OptionSelected === 4){
+  setOptionSelected([4])
+  }
+};
+
+
+console.log('La opción seleccionada es', OptionSelected);
+
+const handleOptionSelected = (ev) => {
+  setOptionSelected(ev.target.value)
+
+};
+
+
+return (
+  <div className='App__background'>
+    <div className="App_layout">
+      <div className="App__mainlayout">
+        <div className="App__sidebar">
+          <SideBar />
         </div>
-    </div>   
-  );
+        <div className="App__main">
+          <p> Jenny eres la mejor del mundo mundial</p>
+
+        </div>
+      </div>
+      <div className="App__footer">
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default App;
---
+++ cuidado-con-grogu/src/components/Main.js
@@ -0,0 +1,10 @@
+import HomePage from "./HomePage";
+
+function main() {
+console.log('entro a main');
+return(
+<p>Hola</p>
+
+);
+};
+export default main;
